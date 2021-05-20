43c9c1afa9d2cae954b577726d1816cd4630815f
Piyush <piyush41y08h@gmail.com>
2021-05-20 14:52:54 +0530

    Fix header padding

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as MenuIcon } from "../../icons/menu.svg";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center px-4 bg-white h-16 fixed top-0 left-0 z-20 w-full">
+    <div className="flex justify-between items-center px-6 bg-white h-16 fixed top-0 left-0 z-20 w-full">
       <div className="flex space-x-6">
         <MenuIcon className="h-6" />
         <Link href="/">

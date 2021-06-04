ae115ed8fa6dd0583d0e90e8073e856d128ccb14
Piyush <piyush41y08h@gmail.com>
2021-06-04 17:14:44 +0530

    Add rating and edit mutations

--- components/Comments2/Comment.tsx
+++ components/Comments2/Comment.tsx
@@ -1,12 +1,109 @@
-import { FC } from "react";
+import axios from "axios";
+import { FC, FormEventHandler, useRef, useState } from "react";
+import { useMutation } from "react-query";
+import { useAuth } from "../../contexts/auth";
 import IComment from "../../interfaces/Comment";
+import IRatings from "../../interfaces/Ratings";
 
 interface Props {
   data: IComment;
 }
 
-const Comment: FC<Props> = ({ data }) => {
-  return <div>{data.text}</div>;
+type RType = "like" | "dislike" | "remove";
+
+const Comment: FC<Props> = (props) => {
+  const { authenticatedAction, user } = useAuth();
+  const [isEditing, setIsEditing] = useState(false);
+  const editInputRef = useRef<HTMLInputElement>(null);
+  const [data, setData] = useState(props.data);
+  const ratingsMutation = useMutation(
+    async (type: RType) => {
+      const baseUrl = `/api/ratings/comments/${data.id}`;
+      switch (type) {
+        case "like":
+          const { data } = await axios.post<IRatings>(`${baseUrl}/like`);
+          return data;
+        case "dislike":
+          const { data: data2 } = await axios.post<IRatings>(
+            `${baseUrl}/dislike`
+          );
+          return data2;
+        case "remove":
+          const { data: data3 } = await axios.delete<IRatings>(`${baseUrl}`);
+          return data3;
+      }
+    },
+    { onSuccess: (ratings) => setData((prev) => ({ ...prev, ratings })) }
+  );
+  const editMutation = useMutation(async (text: string) => {
+    const res = await axios.patch<IComment>(`/api/comments/${data.id}`, {
+      text,
+    });
+    return res.data;
+  });
+
+  // Derived data
+  const hasUserLiked = data.ratings.userRatingStatus === "LIKED";
+  const hasUserDisliked = data.ratings.userRatingStatus === "DISLIKED";
+  const isAuthorUser = data.author.id === user.id;
+
+  // User Actions
+  const onLike = authenticatedAction(() =>
+    ratingsMutation.mutate(hasUserLiked ? "remove" : "like")
+  );
+  const onDislike = authenticatedAction(() =>
+    ratingsMutation.mutate(hasUserDisliked ? "remove" : "dislike")
+  );
+  const toggleEdit = () => setIsEditing((e) => !e);
+  const onEditFormSubmit: FormEventHandler = async (event) => {
+    event.preventDefault();
+    const inputValue = editInputRef.current?.value;
+    if (!inputValue) return;
+
+    const data = await editMutation.mutateAsync(inputValue);
+    setData(data);
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="border border-black my-2">
+      <div className="flex">
+        {isEditing ? (
+          <form onSubmit={onEditFormSubmit}>
+            <input ref={editInputRef} required defaultValue={data.text} />
+            <button className="bg-gray-200 p-1" type="submit">
+              save
+            </button>
+          </form>
+        ) : (
+          <span className="p-2">{data.text}</span>
+        )}
+        <div className="ml-auto">
+          <button
+            disabled={ratingsMutation.isLoading}
+            className="bg-gray-200 p-2"
+            onClick={onLike}
+          >
+            {data.ratings.count.likes} {hasUserLiked ? "👍🏽" : "👍"}
+          </button>
+          <button
+            disabled={ratingsMutation.isLoading}
+            className="bg-gray-200 p-2"
+            onClick={onDislike}
+          >
+            {data.ratings.count.dislikes} {hasUserDisliked ? "👎🏽" : "👎"}
+          </button>
+        </div>
+      </div>
+      <div>
+        {isAuthorUser && (
+          <button onClick={toggleEdit} className="bg-gray-200 p-2">
+            ✏
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Comment;

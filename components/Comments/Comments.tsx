1c9e129b4a8db7c1b7dbb421e5b5746b5686e90f
Piyush <piyush41y08h@gmail.com>
2021-06-06 21:27:20 +0530

    style form and add loader

--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -13,6 +13,7 @@ import { FC, FormEventHandler, useEffect, useRef, useState } from "react";
 import Button from "../Button";
 import Avatar from "../Avatar";
 import Input from "../Input";
+import Loading from "../Loading";
 
 interface Props {
   videoId: number;
@@ -93,37 +94,64 @@ const Comments: FC<Props> = ({ videoId }) => {
 
   return (
     <div className="p-4">
-      {total && <p className="block">{total} Comments</p>}
-
-      <form className="h-20 relative" onSubmit={handleSubmit}>
-        <div className="flex space-x-4">
-          <Avatar src={user?.picture} alt={user?.name} />
-          <Input
-            required
-            ref={inputRef}
-            onClick={() => setIsCommenting(true)}
-            placeholder="Add a public comment..."
-          />
-        </div>
-        {isCommenting && (
+      {total && <p className="block mb-5">{total} Comments</p>}
+      <form className="relative mb-8" onSubmit={handleSubmit}>
+        {commentsMutation.isLoading ? (
+          <Loading />
+        ) : (
           <div>
-            <Button onClick={toggleIsCommenting}>Cancel</Button>
-            <Button type="submit">Comment</Button>
+            <div className="flex space-x-4">
+              <Avatar src={user?.picture} alt={user?.name} />
+              <Input
+                required
+                ref={inputRef}
+                onClick={() => setIsCommenting(true)}
+                placeholder="Add a public comment..."
+              />
+            </div>
+            {isCommenting && (
+              <div className="flex justify-end pt-3 space-x-2">
+                <Button
+                  size="sm"
+                  appearance="none"
+                  className="uppercase font-medium text-secondary"
+                  onClick={toggleIsCommenting}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  size="sm"
+                  type="submit"
+                  appearance="primary"
+                  className="uppercase font-medium"
+                  disabled={!!inputRef?.current?.value}
+                >
+                  Comment
+                </Button>
+              </div>
+            )}
           </div>
         )}
       </form>
-      {newComments.map((newComment) => (
-        <Comment key={newComment.id} data={newComment} onDeleted={onDeleted} />
-      ))}
-      {data?.pages.map((page) =>
-        page.items.map((comment) =>
-          newComments.find(
-            (newComment) => newComment.id === comment.id
-          ) ? null : (
-            <Comment key={comment.id} data={comment} onDeleted={onDeleted} />
+
+      <div>
+        {newComments.map((newComment) => (
+          <Comment
+            key={newComment.id}
+            data={newComment}
+            onDeleted={onDeleted}
+          />
+        ))}
+        {data?.pages.map((page) =>
+          page.items.map((comment) =>
+            newComments.find(
+              (newComment) => newComment.id === comment.id
+            ) ? null : (
+              <Comment key={comment.id} data={comment} onDeleted={onDeleted} />
+            )
           )
-        )
-      )}
+        )}
+      </div>
       <div ref={bottomRef} />
       {isFetchingNextPage && <div>Loading...</div>}
     </div>

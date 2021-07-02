333d8b6929681eb9e54fb5e99b883006f983df87
Piyush <piyush41y08h@gmail.com>
2021-07-02 21:41:13 +0530

    Add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Videos from "../components/Videos";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Home - JsTube</title>
+      </Head>
       <Videos url="/api/videos" />
     </Layout>
   );

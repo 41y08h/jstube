7b37dee63d91a55c18be308bbf19e8cf692f4a73
Piyush <piyush41y08h@gmail.com>
2021-07-02 19:05:41 +0530

    remove deprecation

--- components/VideosList/VideosList.tsx
+++ components/VideosList/VideosList.tsx
@@ -2,10 +2,6 @@ import { FC } from "react";
 import VideoCard from "../VideoCard";
 import { QVideo } from "../../interfaces/Video";
 
-/**
- * Please use `VideoCardsContainer`
- * @deprecated
- */
 const VideosList: FC<{ videos: QVideo[] }> = ({ videos }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-4 2xl:grid-cols-6 2xl:gap-x-5 gap-3 gap-y-0 sm:p-4 items-start">

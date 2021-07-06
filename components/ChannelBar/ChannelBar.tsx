38022329f6e1d8e84409892affafc2b46cac269f
Piyush <piyush41y08h@gmail.com>
2021-07-06 09:11:56 +0530

    Colocate component

--- components/ChannelBar/ChannelBar.tsx
+++ components/ChannelBar/ChannelBar.tsx
@@ -1,10 +1,33 @@
-import { FC, useState, useEffect } from "react";
+import axios from "axios";
 import Link from "next/link";
+import { FC, useState } from "react";
+import { Button } from "@material-ui/core";
+import { useAuth } from "../../contexts/Auth";
 import Avatar from "@material-ui/core/Avatar";
-import formatNumber from "../../lib/formatNumber";
-import SubscribeButton from "./SubscribeButton";
+import red from "@material-ui/core/colors/red";
+import grey from "@material-ui/core/colors/grey";
 import { IChannel } from "../../interfaces/User";
+import formatNumber from "../../lib/formatNumber";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import ISubscribers from "../../interfaces/Subscribers";
+import { useMutation, useQueryClient } from "react-query";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+  },
+  buttonUnsubscribed: {
+    backgroundColor: red[500],
+    color: theme.palette.getContrastText(red[500]),
+    "&:hover": { backgroundColor: red[700] },
+  },
+  buttonSubscribed: {
+    backgroundColor: grey[200],
+    color: grey[600],
+  },
+}));
 
 interface Props {
   channel: IChannel;
@@ -12,6 +35,28 @@ interface Props {
 
 const ChannelBar: FC<Props> = ({ channel }) => {
   const [subscribers, setSubscribers] = useState(channel.subscribers);
+
+  const classes = useStyles();
+  const { authenticate } = useAuth();
+  const queryClient = useQueryClient();
+  const subscribersMutation = useMutation(
+    (unsubscribe: boolean) => {
+      const url = `/api/subscribers/${channel.id}`;
+      return unsubscribe
+        ? axios.delete<ISubscribers>(url).then((res) => res.data)
+        : axios.post<ISubscribers>(url).then((res) => res.data);
+    },
+    {
+      onSuccess(data) {
+        setSubscribers(data);
+        queryClient.invalidateQueries("/api/subscribers/subscriptions");
+      },
+    }
+  );
+
+  const subscribe = authenticate(() =>
+    subscribersMutation.mutate(subscribers.isUserSubscribed)
+  );
 
   return (
     <div className="flex justify-between items-center border-t border-b px-4 py-3 my-4">
@@ -27,11 +72,19 @@ const ChannelBar: FC<Props> = ({ channel }) => {
           </Typography>
         </div>
       </div>
-      <SubscribeButton
-        channelId={channel.id}
-        subscribers={subscribers}
-        setSubscribers={setSubscribers}
-      />
+      <Button
+        disableElevation
+        variant="contained"
+        className={`${classes.button} ${
+          subscribers.isUserSubscribed
+            ? classes.buttonSubscribed
+            : classes.buttonUnsubscribed
+        }`}
+        onClick={subscribe}
+        disabled={subscribersMutation.isLoading}
+      >
+        {subscribers.isUserSubscribed ? "Subscribed" : "Subscribe"}
+      </Button>
     </div>
   );
 };

46f00e5262a2891c699bbaea5611ffb106e20d4a
Piyush <piyush41y08h@gmail.com>
2021-07-07 17:47:52 +0530

    Add `__PROD__` in config

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 export const __DEV__ = process.env.NODE_ENV === 'development'
+export const __PROD__ = process.env.NODE_ENV === 'production'
 
 export const API_URL = __DEV__
   ? 'http://localhost:5000'

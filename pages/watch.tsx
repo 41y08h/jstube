d0b5f67ce6fed1cc74db611b1b6009e90d4ce430
Piyush <piyush41y08h@gmail.com>
2021-07-07 22:53:32 +0530

    Fix styling

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -1,25 +1,25 @@
-import axios from "axios";
-import Head from "next/head";
-import { FC, useState } from "react";
-import { API_URL } from "../config";
-import { GetServerSideProps } from "next";
-import Layout from "../components/Layout";
-import ChannelBar from "../components/ChannelBar";
-import VideoPlayer from "../components/VideoPlayer";
-import VideoDescription from "../components/VideoDescription";
-import { QVideoDetailed } from "../interfaces/Video";
-import Comments from "../components/Comments";
-import Typography from "@material-ui/core/Typography";
-import numberWithCommas from "../lib/numberWithCommas";
-import dateformat from "dateformat";
-import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
-import ThumbDownIcon from "@material-ui/icons/ThumbDown";
-import ReplyIcon from "@material-ui/icons/Reply";
-import { Button } from "@material-ui/core";
-import { Divider } from "@material-ui/core";
+import axios from 'axios'
+import Head from 'next/head'
+import { FC, useState } from 'react'
+import { GetServerSideProps } from 'next'
+import Layout from '../components/Layout'
+import { API_URL } from '../config'
+import ChannelBar from '../components/ChannelBar'
+import VideoPlayer from '../components/VideoPlayer'
+import VideoDescription from '../components/VideoDescription'
+import { QVideoDetailed } from '../interfaces/Video'
+import Comments from '../components/Comments'
+import Typography from '@material-ui/core/Typography'
+import numberWithCommas from '../lib/numberWithCommas'
+import dateformat from 'dateformat'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
+import ThumbDownIcon from '@material-ui/icons/ThumbDown'
+import ReplyIcon from '@material-ui/icons/Reply'
+import { Button } from '@material-ui/core'
+import { Divider } from '@material-ui/core'
 
 interface Props {
-  data: QVideoDetailed;
+  data: QVideoDetailed
 }
 
 const Watch: FC<Props> = ({ data }) => {
@@ -28,74 +28,74 @@ const Watch: FC<Props> = ({ data }) => {
       <Head>
         <title>{data.title} - JS Tube</title>
       </Head>
-      <div className="flex flex-col">
+      <div className='flex flex-col'>
         <VideoPlayer src={data.src} />
         <Typography
-          variant="subtitle1"
-          component="h1"
-          className="pb-4 pt-5 px-5"
+          variant='subtitle1'
+          component='h1'
+          className='pb-4 pt-5 px-5'
         >
           {data.title}
         </Typography>
-        <div className="flex flex-col px-5 space-y-3">
-          <div className="flex items-start space-x-2">
-            <Typography color="secondary" variant="body2" component="span">
+        <div className='flex flex-col px-5 space-y-3'>
+          <div className='flex items-start space-x-2'>
+            <Typography color='secondary' variant='body2' component='span'>
               {numberWithCommas(data.views)} views
             </Typography>
-            <span className="mx-1.5 text-xl text-secondary font-bold leading-none">
+            <span className='mx-1.5 text-xl text-secondary font-bold leading-none'>
               ·
             </span>
-            <Typography color="secondary" variant="body2" component="span">
-              {dateformat(new Date(data.uploadedAt), "dd-mmm-yyyy")}
+            <Typography color='secondary' variant='body2' component='span'>
+              {dateformat(new Date(data.uploadedAt), 'dd-mmm-yyyy')}
             </Typography>
           </div>
-          <div className="flex items-center justify-start text-secondary">
-            <Button color="secondary" startIcon={<ThumbUpAltIcon />}>
+          <div className='flex items-center justify-start'>
+            <Button color='secondary' startIcon={<ThumbUpAltIcon />}>
               {data.ratings.count.likes}
             </Button>
-            <Button color="secondary" startIcon={<ThumbDownIcon />}>
+            <Button color='secondary' startIcon={<ThumbDownIcon />}>
               {data.ratings.count.dislikes}
             </Button>
             <Button
-              color="secondary"
-              startIcon={<ReplyIcon style={{ transform: "scaleX(-1)" }} />}
+              color='secondary'
+              startIcon={<ReplyIcon style={{ transform: 'scaleX(-1)' }} />}
             >
               Share
             </Button>
           </div>
         </div>
-        <div className="my-4">
+        <div className='my-4'>
           <Divider />
-          <div className="px-5 py-3">
+          <div className='px-5 py-3'>
             <ChannelBar channel={data.channel} />
           </div>
           <Divider />
         </div>
-        <div className="px-5 pb-2">
+        <div className='px-5 pb-2'>
           <VideoDescription text={data.description} />
         </div>
         <Divider />
-        <div className="px-5 my-4">
+        <div className='px-5 my-4'>
           <Comments videoId={data.id} />
         </div>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default Watch;
+export default Watch
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async context => {
   try {
-    const videoId = context.query.v;
-    if (!Boolean(videoId)) throw new Error();
+    const videoId = context.query.v
+    if (!Boolean(videoId)) throw new Error()
 
-    const headers = { cookie: context.req.headers.cookie || "" };
-    const config = { headers: context.req ? headers : undefined };
-    const { data } = await axios(`${API_URL}/videos/${videoId}`, config);
+    const headers = { cookie: context.req.headers.cookie || '' }
+    const config = { headers: context.req ? headers : undefined }
+    const { data } = await axios(`${API_URL}/videos/${videoId}`, config)
 
-    return { props: { data } };
+    return { props: { data } }
   } catch (error) {
-    return { redirect: { destination: "/", permanent: false } };
+    return { redirect: { destination: '/', permanent: false } }
   }
-};
+}

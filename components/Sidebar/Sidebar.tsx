7c116884b5416781d8af7417d684bd0a016d1aff
Piyush <piyush41y08h@gmail.com>
2021-07-08 10:42:04 +0530

    Fix key prop in `Sidebar` list

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC } from 'react'
 import {
   List,
   ListItem,
@@ -8,37 +8,37 @@ import {
   SwipeableDrawer,
   IconButton,
   Divider,
-} from "@material-ui/core";
-import items from "./items";
-import Link from "next/link";
-import Subscriptions from "./Subscriptions";
-import { useAuth } from "../../contexts/Auth";
-import MenuIcon from "@material-ui/icons/Menu";
-import Typography from "@material-ui/core/Typography";
-import moreItems from "./moreItems";
-import Footer from "./Footer";
+} from '@material-ui/core'
+import items from './items'
+import Link from 'next/link'
+import Subscriptions from './Subscriptions'
+import { useAuth } from '../../contexts/Auth'
+import MenuIcon from '@material-ui/icons/Menu'
+import Typography from '@material-ui/core/Typography'
+import moreItems from './moreItems'
+import Footer from './Footer'
 
 interface Props {
-  isOpen: boolean;
-  toggleIsOpen(): void;
+  isOpen: boolean
+  toggleIsOpen(): void
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   text: theme.typography.body2,
-  divider: { margin: "12px 0" },
-  icon: { width: 46, minWidth: "unset" },
-  root: { width: 245, overflowX: "hidden" },
+  divider: { margin: '12px 0' },
+  icon: { width: 46, minWidth: 'unset' },
+  root: { width: 245, overflowX: 'hidden' },
   item: { paddingLeft: 24, paddingRight: 24 },
-  topbar: { display: "flex", alignItems: "center", padding: "8px 14px" },
-}));
+  topbar: { display: 'flex', alignItems: 'center', padding: '8px 14px' },
+}))
 
 const Sidebar: FC<Props> = ({ isOpen, toggleIsOpen }) => {
-  const { user: isLoggedIn } = useAuth();
-  const classes = useStyles();
+  const { user: isLoggedIn } = useAuth()
+  const classes = useStyles()
 
   return (
     <SwipeableDrawer
-      anchor="left"
+      anchor='left'
       open={isOpen}
       onOpen={toggleIsOpen}
       onClose={toggleIsOpen}
@@ -48,16 +48,16 @@ const Sidebar: FC<Props> = ({ isOpen, toggleIsOpen }) => {
         <IconButton onClick={toggleIsOpen}>
           <MenuIcon />
         </IconButton>
-        <Link href="/">
+        <Link href='/'>
           <a>
-            <img className="h-5 pl-4" src="/jstube_logo.svg" alt="JsTube" />
+            <img className='h-5 pl-4' src='/jstube_logo.svg' alt='JsTube' />
           </a>
         </Link>
       </div>
       <List>
         {items.map((Item, i) => {
-          if (Item === "divider")
-            return <Divider classes={{ root: classes.divider }} />;
+          if (Item === 'divider')
+            return <Divider key={i} classes={{ root: classes.divider }} />
           else {
             const Component = (
               <Link key={i} href={Item.link}>
@@ -73,21 +73,21 @@ const Sidebar: FC<Props> = ({ isOpen, toggleIsOpen }) => {
                   </ListItem>
                 </a>
               </Link>
-            );
+            )
 
-            return Item.isAuthRequired ? isLoggedIn && Component : Component;
+            return Item.isAuthRequired ? isLoggedIn && Component : Component
           }
         })}
       </List>
       <Subscriptions />
       <Divider classes={{ root: classes.divider }} />
-      <Typography component="span" variant="button" className="px-7">
+      <Typography component='span' variant='button' className='px-7'>
         More from JsTube
       </Typography>
       <List>
         {moreItems.map((Item, i) => {
-          if (Item === "divider")
-            return <Divider classes={{ root: classes.divider }} />;
+          if (Item === 'divider')
+            return <Divider key={i} classes={{ root: classes.divider }} />
           else {
             const Component = (
               <Link key={i} href={Item.link}>
@@ -103,15 +103,15 @@ const Sidebar: FC<Props> = ({ isOpen, toggleIsOpen }) => {
                   </ListItem>
                 </a>
               </Link>
-            );
+            )
 
-            return Item.isAuthRequired ? isLoggedIn && Component : Component;
+            return Item.isAuthRequired ? isLoggedIn && Component : Component
           }
         })}
       </List>
       <Footer />
     </SwipeableDrawer>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar

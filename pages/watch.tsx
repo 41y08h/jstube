760787535dead680234f789f09de3c5fb1092eef
Piyush <piyush41y08h@gmail.com>
2021-07-10 13:49:35 +0530

    Add video ratings mutation

--- pages/watch.tsx
+++ pages/watch.tsx
@@ -1,64 +1,125 @@
 import axios from 'axios'
+import { FC } from 'react'
 import Head from 'next/head'
-import { FC, useState } from 'react'
-import { GetServerSideProps } from 'next'
-import Layout from '../components/Layout'
+import dateformat from 'dateformat'
 import { API_URL } from '../config'
+import Layout from '../components/Layout'
+import { GetServerSideProps } from 'next'
+import { useAuth } from '../contexts/Auth'
+import IRatings from '../interfaces/Ratings'
+import Comments from '../components/Comments'
+import { makeStyles } from '@material-ui/styles'
+import blue from '@material-ui/core/colors/blue'
+import ReplyIcon from '@material-ui/icons/Reply'
 import ChannelBar from '../components/ChannelBar'
 import VideoPlayer from '../components/VideoPlayer'
-import VideoDescription from '../components/VideoDescription'
+import { Button, Divider } from '@material-ui/core'
 import { QVideoDetailed } from '../interfaces/Video'
-import Comments from '../components/Comments'
 import Typography from '@material-ui/core/Typography'
 import numberWithCommas from '../lib/numberWithCommas'
-import dateformat from 'dateformat'
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import ThumbDownIcon from '@material-ui/icons/ThumbDown'
-import ReplyIcon from '@material-ui/icons/Reply'
-import { Button } from '@material-ui/core'
-import { Divider } from '@material-ui/core'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
+import VideoDescription from '../components/VideoDescription'
+import { useMutation, useQueryClient, useQuery } from 'react-query'
+
+const useStyles = makeStyles({ highlightedButton: { color: blue[700] } })
 
 interface Props {
   data: QVideoDetailed
 }
 
-const Watch: FC<Props> = ({ data }) => {
+const Watch: FC<Props> = props => {
+  const classes = useStyles()
+  const { authenticate, user } = useAuth()
+  const queryClient = useQueryClient()
+  const queryKey = `/api/videos/${props.data.id}`
+  const { data } = useQuery(queryKey, { initialData: props.data })
+
+  type RatingType = 'like' | 'dislike' | 'remove'
+
+  const { mutate: rate, isLoading: isRating } = useMutation(
+    (type: RatingType) => {
+      type T = IRatings
+
+      switch (type) {
+        case 'like':
+          return axios.post<T>(`/api/ratings/videos/${data?.id}/like`)
+        case 'dislike':
+          return axios.post<T>(`/api/ratings/videos/${data?.id}/dislike`)
+        case 'remove':
+          return axios.delete<T>(`/api/ratings/videos/${data?.id}`)
+      }
+    },
+    {
+      onSuccess: res => {
+        queryClient.setQueryData<QVideoDetailed>(queryKey, data =>
+          data ? { ...data, ratings: res.data } : undefined
+        )
+      },
+    }
+  )
+
+  const hasUserLiked = data?.ratings.userRatingStatus === 'LIKED'
+  const hasUserDisliked = data?.ratings.userRatingStatus === 'DISLIKED'
+
   return (
     <Layout>
       <Head>
-        <title>{data.title} - JS Tube</title>
+        <title>{data?.title} - JS Tube</title>
       </Head>
       <div className='flex flex-col'>
-        <VideoPlayer src={data.src} />
+        <VideoPlayer src={data?.src} />
         <Typography
           variant='subtitle1'
           component='h1'
           className='pb-4 pt-5 px-5'
         >
-          {data.title}
+          {data?.title}
         </Typography>
         <div className='flex flex-col px-5 space-y-3'>
           <div className='flex items-start space-x-2'>
             <Typography color='secondary' variant='body2' component='span'>
-              {numberWithCommas(data.views)} views
+              {numberWithCommas(data?.views)} views
             </Typography>
             <span className='mx-1.5 text-xl text-secondary font-bold leading-none'>
               ·
             </span>
             <Typography color='secondary' variant='body2' component='span'>
-              {dateformat(new Date(data.uploadedAt), 'dd-mmm-yyyy')}
+              {dateformat(new Date(data?.uploadedAt), 'dd-mmm-yyyy')}
             </Typography>
           </div>
           <div className='flex items-center justify-start'>
-            <Button color='secondary' startIcon={<ThumbUpAltIcon />}>
-              {data.ratings.count.likes}
+            <Button
+              color='secondary'
+              startIcon={
+                <ThumbUpAltIcon
+                  className={hasUserLiked ? classes.highlightedButton : ''}
+                />
+              }
+              onClick={authenticate(() =>
+                rate(hasUserLiked ? 'remove' : 'like')
+              )}
+            >
+              {data?.ratings.count.likes}
             </Button>
-            <Button color='secondary' startIcon={<ThumbDownIcon />}>
-              {data.ratings.count.dislikes}
+            <Button
+              color='secondary'
+              startIcon={
+                <ThumbDownIcon
+                  className={hasUserDisliked ? classes.highlightedButton : ''}
+                />
+              }
+              disabled={isRating}
+              onClick={authenticate(() =>
+                rate(hasUserDisliked ? 'remove' : 'dislike')
+              )}
+            >
+              {data?.ratings.count.dislikes}
             </Button>
             <Button
               color='secondary'
               startIcon={<ReplyIcon style={{ transform: 'scaleX(-1)' }} />}
+              disabled={isRating}
             >
               Share
             </Button>
@@ -67,16 +128,16 @@ const Watch: FC<Props> = ({ data }) => {
         <div className='my-4'>
           <Divider />
           <div className='px-5 py-3'>
-            <ChannelBar channel={data.channel} />
+            <ChannelBar channel={data?.channel} />
           </div>
           <Divider />
         </div>
         <div className='px-5 pb-2'>
-          <VideoDescription text={data.description} />
+          <VideoDescription text={data?.description} />
         </div>
         <Divider />
         <div className='px-5 my-4'>
-          <Comments videoId={data.id} />
+          <Comments videoId={data?.id} />
         </div>
       </div>
     </Layout>

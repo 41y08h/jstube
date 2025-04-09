94cd67f052a720666b3409685c5c7a2f39d4c09f
Piyush <piyush41y08h@gmail.com>
2025-04-09 15:29:41 +0530

    design suggestions

--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { FC, useState } from 'react'
 import dateformat from 'dateformat'
 import { useAuth } from '@/contexts/auth'
@@ -9,13 +9,20 @@ import Comments from '@/components/Comments'
 import ChannelBar from '@/components/ChannelBar'
 import VideoPlayer from '@/components/VideoPlayer'
 import VideoDescription from '@/components/VideoDescription'
-import { Button, Divider, Typography } from '@mui/material'
+import {
+  Button,
+  ButtonBase,
+  Divider,
+  Typography,
+  useTheme,
+} from '@mui/material'
 import ThumbDownIcon from '@mui/icons-material/ThumbDown'
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
 import ReplyIcon from '@mui/icons-material/Reply'
 import { blue } from '@mui/material/colors'
 import numberWithCommas from '@/lib/numberWithCommas'
 import {
+  useInfiniteQuery,
   useMutation,
   useQuery,
   useQueryClient,
@@ -23,9 +30,130 @@ import {
 } from '@tanstack/react-query'
 import { getVideo } from '@/lib/api' // Fetch video data in a separate function
 import { CommentsProvider } from '@/contexts/comments'
+import Videos from '../Videos'
+import CenteredSpinner from '../CenteredSpinner'
+import { QVideos } from '@/interfaces/Video'
+import Link from 'next/link'
+import formatTime from '@/lib/formatTime'
+import formatNumber from '@/lib/formatNumber'
+import timeSince from '@/lib/timeSince'
+import VideoMenu from '../VideoCard/VideoMenu'
 
 interface Props {
   video: any
+}
+
+const Suggestions = () => {
+  const theme = useTheme()
+  const suggestionsQuery = useInfiniteQuery<QVideos, AxiosError>({
+    queryKey: ['/api/videos'],
+    initialPageParam: 1,
+    queryFn: async ({ pageParam }) =>
+      axios('/api/videos', {
+        params: { page: pageParam },
+      }).then(res => res.data),
+    getNextPageParam: lastPage =>
+      lastPage.hasMore ? lastPage.page + 1 : undefined,
+  })
+
+  if (suggestionsQuery.isLoading) return <CenteredSpinner />
+
+  if (suggestionsQuery.isError) return <div>Error loading suggestions</div>
+
+  return (
+    <div className='flex flex-col space-y-2'>
+      {suggestionsQuery.data?.pages.map(page =>
+        page.items.map(video => {
+          const links = {
+            video: `/watch?v=${video.id}`,
+            channel: `/channel/${video.channel.id}`,
+          }
+
+          return (
+            <ButtonBase
+              key={video.id}
+              component='div'
+              className='overflow-hidden'
+              sx={{ mb: 1 }}
+            >
+              <div className='flex w-full pr-6'>
+                <div className='absolute -top-2 right-3'>
+                  <VideoMenu id={video.id} isInWL={video.isInWL} />
+                </div>
+                <div className='relative min-w-[200px] mr-2'>
+                  <Link href={links.video}>
+                    <div className='aspect-ratio'>
+                      <img
+                        src={video.thumbnail}
+                        alt={video.title}
+                        className='rounded-lg'
+                      />
+                    </div>
+                  </Link>
+                  <Typography
+                    variant='caption'
+                    className='absolute bottom-1 right-1 text-white px-1 py-0 rounded font-medium'
+                    style={{
+                      backgroundColor: 'rgb(0, 0, 0, 0.8)',
+                      fontWeight: 500,
+                    }}
+                  >
+                    {formatTime(video.duration)}
+                  </Typography>
+                </div>
+                <div className='flex flex-col'>
+                  <Link href={links.video}>
+                    <Typography
+                      className='line-clamp-2'
+                      variant='subtitle2'
+                      sx={{ fontWeight: theme.typography.fontWeightMedium }}
+                    >
+                      {video.title}
+                    </Typography>
+                  </Link>
+
+                  <div className='text-secondary flex lg:flex-col lg:items-start flex-wrap items-center w-full '>
+                    <Link
+                      href={links.channel}
+                      className='flex items-center space-x-2 pr-2'
+                    >
+                      <Typography variant='caption'>
+                        {video.channel.name}
+                      </Typography>
+
+                      <span
+                        className='material-symbols-outlined ml-1 w-3'
+                        style={{
+                          fontVariationSettings:
+                            "'FILL' 1, 'wght' 200, 'GRAD' 200, 'opsz' 20",
+                          fontSize: '1rem',
+                        }}
+                      >
+                        verified
+                      </span>
+                    </Link>
+                    <div className='flex items-center flex-wrap'>
+                      <Typography variant='caption' component='span'>
+                        {video.views
+                          ? `${formatNumber(video.views)} views`
+                          : 'No views'}
+                      </Typography>
+                      <span className='mx-1.5 text-xl font-bold leading-none'>
+                        ·
+                      </span>
+                      <Typography variant='caption' component='span'>
+                        {timeSince(video.uploadedAt)}
+                      </Typography>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </ButtonBase>
+          )
+        })
+      )}
+    </div>
+  )
 }
 
 const Watch: FC<Props> = ({ video }) => {
@@ -42,8 +170,8 @@ const Watch: FC<Props> = ({ video }) => {
   })
 
   return (
-    <div className='p-6 px-18 flex'>
-      <div className='w-[70%]'>
+    <div className='p-6 px-8 2xl:px-18 flex justify-center space-x-8'>
+      <div className='w-[74.25%]'>
         <VideoPlayer src={data.src} />
         <Typography variant='h6' sx={{ pb: 1.5, pt: 2 }}>
           {data.title}
@@ -66,7 +194,9 @@ const Watch: FC<Props> = ({ video }) => {
           </CommentsProvider>
         </div>
       </div>
-      <div>Suggestions will go here</div>
+      <div className='w-[25.75%]'>
+        <Suggestions />
+      </div>
     </div>
   )
 }

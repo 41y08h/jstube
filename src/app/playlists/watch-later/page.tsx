5194056eb6248e573f68f0078d6827e30cca5989
Piyush <piyush41y08h@gmail.com>
2025-04-13 16:23:49 +0530

    design watch later page

--- src/app/playlists/watch-later/page.tsx
+++ src/app/playlists/watch-later/page.tsx
@@ -1,59 +1,168 @@
 'use client'
+import { LinearProgress } from '@material-ui/core'
 import axios from 'axios'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { Button, Card } from 'react-bootstrap'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { LinearProgress } from '@material-ui/core'
+import { Card, Button } from 'react-bootstrap'
+import {
+  InfiniteData,
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query'
 import Layout from '@/components/Layout'
-
-function WLCard({ data: wl, onDeleted }) {
-  const [isDeleting, setIsDeleting] = useState(false)
-
-  async function remove() {
-    setIsDeleting(true)
-    await axios.delete(`/api/watchlater/${wl.video.id}`)
-    setIsDeleting(false)
-    onDeleted(wl.video.id)
-  }
-
-  if (isDeleting) <div>hmm, i'm being deleted</div>
-
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant='top' src={wl.video.thumbnail} />
-      <Card.Body>
-        <Card.Title>{wl.video.title}</Card.Title>
-        <Button variant='primary' onClick={remove}>
-          Remove
-        </Button>
-      </Card.Body>
-    </Card>
-  )
-}
+import { ButtonBase, Typography, useTheme } from '@mui/material'
+import VideoMenu from '@/components/VideoCard/VideoMenu'
+import formatTime from '@/lib/formatTime'
+import formatNumber from '@/lib/formatNumber'
+import timeSince from '@/lib/timeSince'
+import { QVideosPage } from '@/interfaces/Video'
 
 export default function WatchLater() {
+  const theme = useTheme()
   const queryClient = useQueryClient()
-  const { data, isLoading, isFetching } = useQuery({
+  const { data, isLoading, isFetching } = useInfiniteQuery<QVideosPage>({
     queryKey: ['/api/watchlater'],
+    initialPageParam: 1,
+    queryFn: async ({ pageParam }) =>
+      axios('/api/watchlater', {
+        params: { page: pageParam },
+      }).then(res => res.data),
+    getNextPageParam: lastPage =>
+      lastPage.hasMore ? lastPage.pageNumber + 1 : undefined,
   })
 
-  function handleDelete(videoId) {
-    queryClient.setQueryData(['/api/watchlater'], old =>
-      old.filter(wl => wl.video.id !== videoId)
+  function updateIsInWL(videoId: number) {
+    queryClient.setQueryData<InfiniteData<QVideosPage>>(
+      ['/api/watchlater'],
+      data => {
+        if (!data) return data
+        const newData = { ...data }
+        const pages = newData.pages.map(page => {
+          const videos = page.items.filter(video => video.id != videoId)
+          return { ...page, items: videos }
+        })
+        return { ...newData, pages }
+      }
     )
   }
 
-  if (isLoading) return <>Loading...</>
+  if (isLoading) return <Layout />
 
   return (
     <Layout>
-      <div>
-        {isFetching && <LinearProgress />}
-        <Link href='/'>back</Link>
-        {data.map(wl => (
-          <WLCard data={wl} onDeleted={handleDelete} />
-        ))}
+      <div className='px-18 py-6'>
+        <Typography
+          variant='h4'
+          sx={{ fontWeight: theme.typography.fontWeightBold }}
+        >
+          Watch Later
+        </Typography>
+        <div className='flex flex-col mt-8'>
+          {data?.pages.map(page =>
+            page.items.map(video => {
+              const links = {
+                video: `/watch?v=${video.id}`,
+                channel: `/channel/${video.channel.id}`,
+              }
+
+              return (
+                <ButtonBase
+                  key={video.id}
+                  component='div'
+                  className='overflow-hidden'
+                  sx={{ mb: 2 }}
+                >
+                  <div className='flex w-full pr-6'>
+                    <div className='absolute -top-2 right-3'>
+                      <VideoMenu
+                        id={video.id}
+                        isInWL={video.isInWL}
+                        updateIsInWL={updateIsInWL}
+                      />
+                    </div>
+                    <div className='relative min-w-[260px] mr-4'>
+                      <Link href={links.video}>
+                        <div className='aspect-ratio'>
+                          <img
+                            src={video.thumbnail}
+                            alt={video.title}
+                            className='rounded-lg'
+                          />
+                        </div>
+                      </Link>
+                      <Typography
+                        variant='caption'
+                        className='absolute bottom-1 right-1 text-white px-1 py-0 rounded font-medium'
+                        style={{
+                          backgroundColor: 'rgb(0, 0, 0, 0.8)',
+                          fontWeight: 500,
+                        }}
+                      >
+                        {formatTime(video.duration)}
+                      </Typography>
+                    </div>
+                    <div className='flex flex-col'>
+                      <Link href={links.video}>
+                        <Typography
+                          className='line-clamp-2'
+                          sx={{
+                            fontSize: '1.125rem',
+                            fontWeight: theme.typography.fontWeightMedium,
+                          }}
+                        >
+                          {video.title}
+                        </Typography>
+                      </Link>
+
+                      <div className='text-secondary flex lg:flex-col lg:items-start flex-wrap items-center w-full '>
+                        <Link
+                          href={links.channel}
+                          className='flex items-center space-x-2 pr-2'
+                        >
+                          <Typography variant='caption'>
+                            {video.channel.name}
+                          </Typography>
+
+                          <span
+                            className='material-symbols-outlined ml-1 w-3'
+                            style={{
+                              fontVariationSettings:
+                                "'FILL' 1, 'wght' 200, 'GRAD' 200, 'opsz' 20",
+                              fontSize: '1rem',
+                            }}
+                          >
+                            verified
+                          </span>
+                        </Link>
+                        <div className='flex items-center flex-wrap'>
+                          <Typography variant='caption' component='span'>
+                            {video.views
+                              ? `${formatNumber(video.views)} views`
+                              : 'No views'}
+                          </Typography>
+                          <span className='mx-1.5 text-xl font-bold leading-none'>
+                            ·
+                          </span>
+                          <Typography variant='caption' component='span'>
+                            {timeSince(video.uploadedAt)}
+                          </Typography>
+                        </div>
+                        <Typography
+                          variant='caption'
+                          className='line-clamp-3'
+                          sx={{ mt: 1 }}
+                        >
+                          {video.description}
+                        </Typography>
+                      </div>
+                    </div>
+                  </div>
+                </ButtonBase>
+              )
+            })
+          )}
+        </div>
       </div>
     </Layout>
   )
